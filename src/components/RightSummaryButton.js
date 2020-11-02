6a26ce9312d7dc2374e21ff42029ad360fd60fd5
Jonesy22 <Jonesy22@users.noreply.github.com>
2020-11-02 11:47:57 -0800

    Adding summary button, followed LeftProjectButton example

---
+++ src/components/RightSummaryButton.js
@@ -0,0 +1,13 @@
+import Button from 'react-bootstrap/Button'
+import React, {useState, useEffect } from 'react'
+
+const RightSummaryButton = ({}) => {
+
+    return (
+        <div>
+            <Button variant="primary">Summary</Button>{' '}
+        </div>
+    );          
+}
+
+export default RightSummaryButton
