a5fb082d646c7bd442ab0fa0624630325687e3d3
trevor-jones <trevor.eric.j@gmail.com>
2020-11-16 13:48:42 -0800

    fixed class->className

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return(
-        <footer class="page-footer font-small blue">
-        <div class="footer-copyright text-center py-3">© 2020 Copyright: Perro
+        <footer className="page-footer font-small blue">
+        <div className="footer-copyright text-center py-3">© 2020 Copyright: Perro
         </div>
         </footer>
     );
