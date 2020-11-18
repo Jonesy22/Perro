ceb14be720d9323f5edb4049506c9b5c40672eb7
Jonesy22 <Jonesy22@users.noreply.github.com>
2020-11-18 14:57:48 -0800

    Axis labels too tiny :(

--- src/components/GraphView.jsx
+++ src/components/GraphView.jsx
@@ -1,24 +1,45 @@
 import React, {useState, useEffect } from 'react'
-import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from 'react-vis';
+import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries, ChartLabel} from 'react-vis';
 
 class GraphView extends React.Component {
     render() {
 	return (
-            <div>
-               <XYPlot
- 		 width={300}
-  		 height={300}>
-  		 <HorizontalGridLines />
-  		 <LineSeries
+        <div>
+            <XYPlot
+ 		 		width={800}
+  		 		height={500}
+				margin={{bottom: 80, left: 50, right: 10, top: 20}} >
+  		 	<HorizontalGridLines />
+  		 	<LineSeries
     			data={[
       			{x: 1, y: 10},
       			{x: 2, y: 5},
       			{x: 3, y: 15}
-    			]}/>
-  		 <XAxis />
-  		 <YAxis />
-		</XYPlot>		
-            </div>
+    		]}/>
+  			<XAxis />
+			<YAxis />
+		   	<ChartLabel
+				text="Time Estimate(Days)"
+				xPercent={.45}
+    			yPercent={.85}
+    			className="alt-x-label"
+				style={{
+					title: {font: '45px'}
+				}}
+			/>
+			<ChartLabel
+				text="Days Work"
+				xPercent={.01}
+    			yPercent={.3}
+    			className="alt-y-label"
+				style={{
+					title: {size: '45px'},
+					transform: 'rotate(-90)',
+					textAnchor: 'end'
+				}}
+			/>
+			</XYPlot>		
+        </div>
         )
     }
 }
--- src/components/TrackingView.jsx
+++ src/components/TrackingView.jsx
@@ -1,23 +1,13 @@
 import React, {useState, useEffect } from 'react'
+import GraphView from './GraphView'
 import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from 'react-vis';
 
 class TrackingView extends React.Component {
     render() {
         return (
             <div>
-                <XYPlot
-  		width={300}
-  		height={300}>
-  		<HorizontalGridLines />
-  		<LineSeries
-    		data={[
-      			{x: 1, y: 10},
-      			{x: 2, y: 5},
-      			{x: 3, y: 15}
-    			]}/>
-  		<XAxis />
-  		<YAxis />
-		</XYPlot>
+                <h2>Tracking View</h2>
+				<GraphView />
             </div>
         )
     }
