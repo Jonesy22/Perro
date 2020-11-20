0e6c17e379827c0b915c747d22bb1dd04b7986f8
Matthew Levis <matthewlev13@comcast.net>
2020-11-20 10:15:39 -0800

    landing page added, no good styling yet or button to get to tracking.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 import 'react-bootstrap'
+import React from 'react';
 // components
 import LeftProjectView from './components/LeftProjectView'
 import RightProjectView from './components/RightProjectView'
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import LandingPage from './components/LandingPage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  } from "react-router-dom"
 
-function App() {
+let LandingPageComponent = props => <LandingPage/>
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+          <Switch>
+            <Route path="/" exact component={() => <LandingPage />}/>
+            <Route path="/tracking" exact component={TrackingView}/>
+          </Switch>
+      </Router>
+
+    );
+  }
+  
+}
+
+const TrackingView = () => {
   return (
     <div>
       <div className="float-container">
@@ -22,8 +46,7 @@ function App() {
       </div>
       <Footer />
     </div>
-
-  );
+  )
 }
 
 export default App;
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React, {useState, useEffect } from 'react'
+const { Layout, Content } = 'antd'
+
+
+class LandingPage extends React.Component {
+    render() {
+        return (
+            <Layout>
+                <Content>Perro</Content>
+            </Layout>
+        )
+    }
+}
+
+export default LandingPage
