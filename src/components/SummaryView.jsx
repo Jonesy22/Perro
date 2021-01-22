a936f9edfe8a94567ec069aef1e0e66a95452737
Jonesy22 <Jonesy22@users.noreply.github.com>
2021-01-21 16:26:44 -0800

    Updated UI

--- src/components/SummaryView.jsx
+++ src/components/SummaryView.jsx
@@ -7,15 +7,18 @@ class SummaryView extends React.Component {
 		super(props);
         this.state = {milestones: []};
         this.printSummary = this.printSummary.bind(this);
+        this.printName = this.printName.bind(this);
     }
     printSummary() {
-        
        return (this.props.selectedTask.content.Summary);
     }
+    printName() {
+        return (this.props.selectedTask.content.Name);
+     }
     render() {
         return (
             <div>
-                <h2><b>Summary</b></h2>
+                <h2><b>{this.printName()}</b></h2>
                 {this.printSummary()}
             </div>
         )
