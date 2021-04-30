4a9aa23ceb8559cb040bca7a11e95197f7d60f34
Matthew Levis <matthewlev13@comcast.net>
2021-04-29 21:27:49 -0700

    tasks are populated under a search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,26 +5,31 @@ import { addTeam, addMember, removeMember, addTeamToUser, removeTeamFromUser } f
 import {matchSorter} from 'match-sorter'
 import { useThrottle } from "react-use";
 import { createMatchSelector } from "connected-react-router";
+import { connect } from 'react-redux'
 import {  Combobox,  ComboboxInput,  ComboboxPopover,  ComboboxList,  ComboboxOption,  ComboboxOptionText,} from "@reach/combobox";
 import "@reach/combobox/styles.css";
+import { getTasks } from "../data/selectors";
 
 const searchExamples = [
     "My First Project",
 	"Project 2",
 ]
 
-function SearchBar() {
+function SearchBar(props) {
 	const [term, setTerm] = React.useState("");
     //const results = useCityMatch(term);
     const handleChange = (event) => setTerm(event.target.value);
+	console.log("SEARCH!!!!! :", props);
 	return (
 	  <div>
 		<Combobox aria-labelledby="search box" >
 		  <ComboboxInput placeholder="Search for a task..."/>
 		  <ComboboxPopover>
 			<ComboboxList>
-				<ComboboxOption value="My First Project" />
-				<ComboboxOption value="Project 2" />
+				{props.tasks.map((task) => {
+					const taskName = task.content.Name;
+					return <ComboboxOption key={taskName} value={taskName} />
+				})}
 			</ComboboxList>
 		  </ComboboxPopover>
 		</Combobox>
@@ -33,4 +38,7 @@ function SearchBar() {
   }
 
   
-export default SearchBar
+export default connect(
+	state => ({ tasks: getTasks(state) }),
+	{}
+)(SearchBar);
