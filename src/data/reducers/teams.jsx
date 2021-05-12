792a5812bb312b3e9b98adfacde481ebfaeab086
Trevor-Jones <trevor.eric.j@gmail.com>
2021-05-12 13:48:03 -0700

    added backend routes and connected to redux for users and invitations

--- src/data/reducers/teams.jsx
+++ src/data/reducers/teams.jsx
@@ -1,12 +1,13 @@
 import InputForm from "../../components/InputForm.js";
-import { ADD_TEAM, ADD_MEMBER, REMOVE_MEMBER, UPDATE_TEAMS_TEAMSTATUS, DELETE_TEAM} from "../actionTypes.js";
+import { ADD_TEAM, ADD_MEMBER, REMOVE_MEMBER, UPDATE_TEAMS_TEAMSTATUS, DELETE_TEAM, DELETE_INVITATION, ADD_TEAMS_LIST, ADD_INV_LIST} from "../actionTypes.js";
 import { createTeam } from "../createObjects.js";
 
 const initialState = {
+    invitations: [],
     allIds: [0], // list of ids of all the tasks that are loaded
     byIds: {
         // array should be an array of objects (essenetially tuples with a userId and a accepted boolean)
-        0: {content :createTeam("Perro","Default User", [])}, 
+        // 0: {content :createTeam("Perro","Default User", [{userId: "jonetrev@oregonstate.edu", teamStatus: false}], 0)}, 
     }
 }
 
@@ -26,6 +27,22 @@ const executeAction = function(state = initialState, action) {
             };
         }
 
+        case ADD_TEAMS_LIST: {
+            const { content } = action.payload;
+            let ids = [];
+            for(let team in content) {
+                ids.push(team.teamID);
+            }
+            return {
+                ...state,
+                allIds: [...state.allIds, ...ids],
+                byIds: {
+                    ...state.byIds,
+                    ...content
+                }
+            };
+        }
+        
         case DELETE_TEAM: {
             const { teamId } = action.payload;
             let teams = {...state.byIds}
@@ -41,8 +58,31 @@ const executeAction = function(state = initialState, action) {
             };
         }
 
+        case DELETE_INVITATION: {
+            const {userEmail, teamId} = action.payload;
+            let updatedInvs = [...state.invitations];
+            for(let i = 0; i < updatedInvs.length; i++) {
+                if(updatedInvs[i].teamId === teamId) {
+                    updatedInvs.splice(i, 1);
+                    i--;
+                }
+            }
+            return {
+                ...state,
+                invitations: updatedInvs
+            };
+        }
+
+        case ADD_INV_LIST: {
+            const {content} = action.payload;
+            return {
+                ...state,
+                invitations: [...state.invitations, ...content]
+            };
+        }
+
         case ADD_MEMBER: {
-            const {userId, teamId, nextUserId} = action.payload;
+            const {userId, teamId, accepted} = action.payload;
             let teams = {...state.byIds}
             let dupCheck = false;
             Object.entries(teams[teamId].content.teamMembers).map(
@@ -56,7 +96,7 @@ const executeAction = function(state = initialState, action) {
                 console.log("Member has already been added")
             }
             else{
-                teams[teamId].content.teamMembers.push({userId: userId, teamStatus: false})
+                teams[teamId].content.teamMembers.push({userId: userId, teamStatus: accepted})
             }
             return {
                 ...state,
