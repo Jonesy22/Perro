b9d2e9f767cae02a1f87019d083da41cf53f8359
Kyler Jacobson <kylerjacobson@protonmail.com>
2021-05-12 13:21:39 -0700

    conditional rendering of delete button

--- src/components/SettingsView.jsx
+++ src/components/SettingsView.jsx
@@ -52,6 +52,30 @@ class SettingsView extends React.Component {
             fontSize: "14px",
         };
 
+        function RenderDeleteButton(props){
+
+            console.log(props.teamLead)
+            console.log(props.userId)
+            if (props.teamLead != props.userId){
+                return ''
+            }
+            else{
+                return(
+                <span>
+                <Button
+                    onClick={() => {
+                        props.deleteTeam(props.teamId);
+                    }}
+                    variant="danger"
+                >
+                    Delete
+                </Button>
+            </span>
+                );
+            }
+
+        }
+
         function NotifcationRenderer(props) {
             const notifications = props.notificationsBool;
             console.log(props.userId)
@@ -162,7 +186,8 @@ class SettingsView extends React.Component {
                                                         Edit
                                                     </Button>
                                                 </span>
-                                                <span>
+                                                <RenderDeleteButton teamLead={team[1].content.teamLead} teamId={team[0]} userId={this.props.appData.userProfile.tS} deleteTeam={this.props.deleteTeam}/>
+                                                {/* <span>
                                                     <Button
                                                         onClick={() => {
                                                             this.props.removeTeamDB(team[0]);
@@ -171,7 +196,7 @@ class SettingsView extends React.Component {
                                                     >
                                                         Delete
                                                     </Button>
-                                                </span>
+                                                </span> */}
                                             </td>
                                         </tr>
                                     );
